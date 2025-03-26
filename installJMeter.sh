f22fc2960de4c9070b8ca33536ea2d8dc051bbcd
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-26 10:50:30 +0530

    Create installJMeter.sh

---
+++ installJMeter.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+java -version
+wget https://downloads.apache.org//jmeter/binaries/apache-jmeter-5.6.3.tgz
+tar -xvzf apache-jmeter-5.6.3.tgz
+cd apache-jmeter-5.6.3
+./bin/jmeter -v
+
