e2e10433a735cc6e38561ea390511db8b9fc1a70
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-26 11:50:37 +0530

    Update installJMeter.sh

--- installJMeter.sh
+++ installJMeter.sh
@@ -1,5 +1,4 @@
-sudo apt update
-sudo apt install openjdk-11-jdk -y
+sudo apt update && sudo apt install openjdk-11-jdk -y && sudo apt install apache2 -y
 java -version
 wget https://downloads.apache.org//jmeter/binaries/apache-jmeter-5.6.3.tgz
 tar -xvzf apache-jmeter-5.6.3.tgz
